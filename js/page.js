a9dd9e1ee7fbdd4dc1ff53144ee31251d5bf7561
wellpinho <wellpinho@outlook.com>
2021-12-13 19:44:11 -0300

    ✨ NEW FEATURE: Chart Bars

---
+++ js/page.js
@@ -0,0 +1,30 @@
+// charts
+let ctx = document.getElementById('montlySales').getContext('2d')
+let pieCtx = document.getElementById('deptSales').getContext('2d')
+
+// bar chart
+let montlySalesChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Oct', 'Nov', 'Dec'],
+    datasets: [{
+      label: '# of Sales',
+      data: [12, 19, 3],
+      backgroundColor: [
+        'rgba(238, 184, 104, 1)',
+        'rgba(75, 166, 223, 1)',
+        'rgba(239, 118, 122, 1)',
+      ],
+      bordarWidth: 0
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+})
