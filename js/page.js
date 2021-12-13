83502f684256ebd35e0aea7eb8a861c9a4d89738
wellpinho <wellpinho@outlook.com>
2021-12-13 20:37:14 -0300

    🔨 REFACTOR:  fill

--- js/page.js
+++ js/page.js
@@ -1,12 +1,49 @@
 // charts
 let ctx = document.getElementById('montlySales').getContext('2d')
 let pieCtx = document.getElementById('deptSales').getContext('2d')
+let yearTotalLabel = document.getElementById('yearlyTotal')
 
 let montlyLabels = Array.of('Oct', 'Nov', 'Dec')
-let montlySales = Array.of(12, 9, 3)
+let montlySales = Array.of(500, 9000, 3000, 400)
 
-let deptLabels = Array.of('Oct', 'Nov', 'Dec')
+let deptLabels = Array.of('Hiking', 'Running', 'Hunting')
 let dptSales = Array.of(12, 9, 3)
+
+let yearlyTotal = 0
+
+function addYearlyTotal(x) {
+  yearlyTotal = x + yearlyTotal
+}
+montlySales.forEach(addYearlyTotal)
+
+let octNums = Array.of(500, 1000, 9000)
+let novNums = Array.of(1100, 2000, 9000)
+let decNums = Array.of(4000, 1000, 5000)
+
+// 1200,1000,9000,1100,2000,9000,4000,1000,5000
+// let total = Array.of(
+//   addYearlyTotal(...octNums),
+//   addYearlyTotal(...novNums),
+//   addYearlyTotal(...decNums)
+// )
+// alert(addYearlyTotal(...total))
+
+// spread operator
+// let yearlyTotal = addYearlyTotal(...montlySales)
+yearTotalLabel.innerHTML = "$" + yearlyTotal
+
+function findOver1000() {
+  // find retorna o primeiro que ele encontrar que satisfaça a busca
+  // findIndex retorna a posição do primeiro que satisfaz a busca
+  let firstThousand = montlySales.findIndex(element => element > 1000)
+  alert(firstThousand)
+}
+
+// reset to zero all chart bars
+function resetNumbers() {
+  montlySales.fill(0)
+  montlySalesChart.update()
+}
 
 // bar chart
 let montlySalesChart = new Chart(ctx, {
